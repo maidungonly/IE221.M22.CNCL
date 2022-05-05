67773aa9d92dd99f9baa2363bb08d9c996d89f6f
ngocduong777 <baongocduong1@gmail.com>
2022-05-05 14:13:07 +0700

    Add static folder and some fonts

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("<int:id>",views.index,name="index"),
-    path("",views.base,name="base"),
+    path("",views.home,name="home"),
     path("create/",views.create,name="create"),
 ]
  
