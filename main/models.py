5efd9843a7edfc447e4c72609c26679ce74564ad
ngocduong777 <baongocduong1@gmail.com>
2022-05-07 22:28:36 +0700

    Them table Kcook Post

--- main/models.py
+++ main/models.py
@@ -1,16 +1,26 @@
 from django.db import models
-from django.forms import CharField
 
 # Create your models here.
-class ToDoList(models.Model):
-    name = models.CharField(max_length=200)
+# class ToDoList(models.Model):
+#     name = models.CharField(max_length=200)
+
+#     def __str__(self):
+#         return self.name
+
+# class Item (models.Model):
+#     todolist = models.ForeignKey(ToDoList,on_delete=models.CASCADE)
+#     text = models.CharField(max_length=300)
+#     complete = models.BooleanField()
+#     def __str__(self):
+#         return self.text
+
+class KcookPost(models.Model):
+    title_post=models.TextField()
+    image = models.ImageField(upload_to = "kcookpost_imgs/")
+    nguyenlieu = models.TextField()
+    introduction = models.CharField(max_length=350,null=True)
+    cachlam = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.title_post
 
-class Item (models.Model):
-    todolist = models.ForeignKey(ToDoList,on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    complete = models.BooleanField()
-    def __str__(self):
-        return self.text
