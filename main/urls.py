ef7a98d64416bbc57bf262bb555a4d30cb816182
ngocduong777 <baongocduong1@gmail.com>
2022-05-10 09:47:40 +0700

    fetch data cho cac muc menu

--- main/urls.py
+++ main/urls.py
@@ -11,11 +11,18 @@ urlpatterns = [
     path("giohang/",views.giohang,name="giohang"),
     path("kcook/",views.kcook,name="kcook"),
     path("giavisot/",views.giavisot,name="giavisot"),
+
+    # path("giavisot/giavisot-list/<slug:slug>",views.giavisot,name="giavisot"),
     path("thucphamanlien/",views.thucphamanlien,name="thucphamanlien"),
+    path("douong/",views.douong,name="douong"),
+    path("banhkeo/",views.banhkeo,name="banhkeo"),
+    path("rongbien/",views.rongbien,name="rongbien"),
+
     path('kcook/kcook-post-list/<int:id>',views.chitietkcook,name='chitietkcook'),
     # path("kcook/chitietkcook/", views.chitietkcook, name="chitietkcook"),
     path("thongtinmuahang/", views.thongtinmh, name="thongtinmuahang"),
     path('chitietsanpham-list/<slug:slug>', views.chitietsanpham, name="chitietsanpham")
+
 ]
  
 if settings.DEBUG:  
--- main/views.py
+++ main/views.py
@@ -65,26 +65,33 @@ def chitietkcook(response,id):
     return render(response,"main/chitietkcook.html",{'data':kcook_post})
 
 def home(response):
-    data = Product.objects.filter(pk__in = [14,15,16,19,18,22,27,28,29,24,25,30,10,11,13])
+    # data = Product.objects.filter(pk__in = [14,15,16,19,18,22,27,28,29,24,25,30,10,11,13])
+    data = Product.objects.all()
+
     return render (response,"main/home.html",{'data':data})
 
 def chitietsanpham(response,slug):
-    data = Product.objects.filter(pk__in = [14,15,16,19,18,22,27,28,29,24,25,30,10,11,13]).get(slug=slug)
+    data = Product.objects.get(slug=slug)
     # chitietsanpham_post = Product.objects.get(id=id)
     # chitietsanpham_post = get_object_or_404(Product, slug=slug)
     # data = Product.objects.get(slug=slug)
     return render(response,"main/chitietsanpham.html",{'data':data})
 
-
-
-
 def thucphamanlien(response):
-    return render(response,"main/thucphamanlien.html",{})
-
+    data = Product.objects.filter(cat_name = 6)
+    return render(response,"main/thucphamanlien.html",{'data':data})
 def giavisot(response):
     data = Product.objects.filter(cat_name = 2)
     return render(response,"main/giavisot.html",{'data':data})
-
+def douong(response):
+    data = Product.objects.filter(cat_name = 4)
+    return render(response,"main/douong.html",{'data':data})
+def banhkeo(response):
+    data = Product.objects.filter(cat_name = 1)
+    return render(response,"main/banhkeo.html",{'data':data})
+def rongbien(response):
+    data = Product.objects.filter(cat_name = 5)
+    return render(response,"main/rongbien.html",{'data':data})
 def thongtinmh(response):
     return render(response,"main/thongtinmh.html",{})
 
