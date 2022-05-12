027e26140593b6339072112a63dc4a00bfa3ecb7
ngocduong777 <baongocduong1@gmail.com>
2022-05-12 13:04:12 +0700

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ STATIC_URL = '/static/'
 STATIC_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
